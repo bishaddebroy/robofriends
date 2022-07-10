21a3f48cfbd9765196181a1423a2c62e07b42a10
Utsha <bishad.deb@sjinnovation.com>
2022-07-11 00:10:40 +0600

    building react app 1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react'; // webpack for bundling and other packages underneath
 import ReactDOM from 'react-dom/client'; // import the virtual DOM from React, it is for websites. react native for mobles.
 import './index.css'; // ./ means in the same directory as the index.js 
 //import App from './App'; // if no type after the file, it is javascript by default. components are capitalized.
-import Hello from './Hello'; // first component
+import Card from './Card'; // first component
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hello greetings={`Hello React Ninja`}/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
   </React.StrictMode>
 );
 
