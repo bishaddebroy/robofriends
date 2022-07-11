1c36cbcec966c3f826d1d28387d71ba75a899b14
Utsha <bishad.deb@sjinnovation.com>
2022-07-11 16:06:12 +0600

    building react app 3

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => { //arrow function makes sure that 'this' is for this class.
+        this.setState({searchfield: event.target.value});
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange} />
+                <CardList robots = { filteredRobots } />
+            </div>
+        );
+    }
+    
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'; // webpack for bundling and other packages underneath the react. it is needed for all the dependencies.
 import ReactDOM from 'react-dom/client'; // import the virtual DOM from React, it is for websites. react native for mobles.
 import './index.css'; // ./ means in the same directory as the index.js 
-//import App from './App'; // if no type after the file, it is javascript by default. components are capitalized.
-//import Card from './Card'; // first component
-import CardList from './CardList';
+import App from './App'; // if no type after the file, it is javascript by default. components are capitalized.
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots = {robots} />
+    <App />
   </React.StrictMode>
 );
 
